6b2f927c8b8234e1748e1b705cf3d84e637b1604
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-23 11:29:03 +0100

    Make CSS work and first component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,66 @@
+
+const Footer = () => {
+    return (
+        <>
+            <div className="frame-16">
+            <div className="frame-12">
+                <img className="logo-2" alt="Logo" src="/img/logo-1.png" />
+                <img className="add" alt="Frame" src="/img/frame-3352.svg" />
+                <p className="text-wrapper-9">Rendez physique vos œuvres digital</p>
+            </div>
+            <div className="frame-14">
+                <div className="group-9">
+                <div className="text-wrapper-10">Nos pages</div>
+                <div className="text-wrapper-11">Accueil</div>
+                <div className="text-wrapper-12">A propos</div>
+                <div className="text-wrapper-13">Marketplace</div>
+                <div className="text-wrapper-14">FAQ</div>
+                </div>
+                <div className="group-10">
+                <div className="text-wrapper-10">Entreprise</div>
+                <div className="text-wrapper-11">Equipes</div>
+                <div className="text-wrapper-12">Partenaire</div>
+                <div className="text-wrapper-13">CGU</div>
+                </div>
+                <img className="line-2" alt="Line" src="/img/line-5.svg" />
+                <div className="group-11">
+                <div className="text-wrapper-10">Contact</div>
+                <p className="text-wrapper-11">02 00 00 00 00</p>
+                <div className="text-wrapper-12">01 Rue Dory, Paris</div>
+                <div className="text-wrapper-13">Mail@gmail.com</div>
+                </div>
+            </div>
+            </div>
+            <header className="header">
+            <img className="logo-2" alt="Logo" src="/img/logo.png" />
+            <div className="wrapper-link">
+                <div className="div-2">
+                <div className="add" />
+                <div className="link-text">
+                    <div className="text-wrapper-20">Communauté</div>
+                </div>
+                <div className="link-text">
+                    <div className="text-wrapper-20">Equipe</div>
+                </div>
+                <div className="link-text">
+                    <div className="text-wrapper-20">A propos</div>
+                </div>
+                <div className="link-text">
+                    <div className="text-wrapper-20">Ressources</div>
+                </div>
+                </div>
+                <div className="div-2">
+                <button className="button-5">
+                    <div className="text-wrapper-5">prévente</div>
+                </button>
+                <button className="button">
+                    <div className="text-wrapper-5">testnet</div>
+                </button>
+                </div>
+            </div>
+            </header>
+        </>
+    )
+}
+
+export default Footer
