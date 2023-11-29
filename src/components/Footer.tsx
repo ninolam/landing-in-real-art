cf270668a5a99f8216a4ce481191e4eeaa28ffcc
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-29 19:10:51 +0100

    Handlke footer gcp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,10 +12,11 @@ import { storage } from "../firebaseConfig";
 const Footer = () => {
 
     //Get the language of the global context
-    const {lang, setLang} = useAppContext()
+    const {lang } = useAppContext()
 
     const FIREBASE_FOOTER_COLLECTION = 'Footer'
 
+    const [contactTitle, setContactTitle] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [phone, setPhone] = useState<string>('');
     const [address, setAddress] = useState<string>('');
@@ -26,6 +27,27 @@ const Footer = () => {
     const [linkedInUrl, setLinkedInUrl]     = useState("")
     const [instagramUrl, setInstagramUrl]   = useState("")
     const [leftBlockText, setleftBlockText] = useState("")
+
+    const [ourPagesTitle, setOurPagesTitle]     = useState("")
+    const [ourCompanyTitle, setOurCompanyTitle] = useState("")
+
+    const [homeLinkText, setHomeLinkText]   = useState("")
+    const [homeLinkUrl, setHomeLinkUrl]     = useState("")
+
+    const [aboutLinkText, setAboutLinkText] = useState("")
+    const [aboutLinkUrl, setAboutLinkUrl]   = useState("")
+
+    const [marketPlaceLinkText, setMarketPlaceLinkText] = useState("")
+    const [marketPlaceLinkUrl, setMarketPlaceLinkUrl] = useState("")
+
+    const [faqLinkText, setFaqLinkText] = useState("")
+    const [faqLinkUrl, setFaqLinkUrl]   = useState("")
+
+    const [teamLinkText, setTeamLinkText] = useState("")
+    const [teamLinkUrl, setTeamLinkUrl]   = useState("")
+
+    const [partnersLinkText, setPartnersLinkText] = useState("")
+    const [partnersLinkUrl, setpartnersLinkUrl]   = useState("")
 
     useEffect(() => {
         const fetchData = async () => {
@@ -48,14 +70,32 @@ const Footer = () => {
             setLinkedInUrl(dataList[1]['linkedInUrl'])
             setleftBlockText(dataList[1]['text'][lang])
 
+            //Links block
+            setOurPagesTitle(dataList[2]['ourPagesTitle'][lang])
+            setOurCompanyTitle(dataList[2]['ourCompanyTitle'][lang])
+            setHomeLinkText(dataList[2]['homeLinkText'][lang])
+            setAboutLinkText(dataList[2]['aboutLinkText'][lang])
+            setMarketPlaceLinkText(dataList[2]['marketPlaceLinkText'][lang])
+            setFaqLinkText(dataList[2]['faqLinkText'][lang])
+            setTeamLinkText(dataList[2]['teamLinkText'][lang])
+            setPartnersLinkText(dataList[2]['partnersLinkText'][lang])
+
+            setHomeLinkUrl(dataList[2]['homeLinkUrl'])
+            setAboutLinkUrl(dataList[2]['aboutLinkUrl'])
+            setMarketPlaceLinkUrl(dataList[2]['marketPlaceLinkUrl'])
+            setFaqLinkUrl(dataList[2]['faqLinkUrl'])
+            setTeamLinkUrl(dataList[2]['teamLinkUrl'])
+            setpartnersLinkUrl(dataList[2]['partnersLinkUrl'])
+
             //Right block  
+            const contactTitle = dataList[0]['contactTitle'][lang];
             const email   = dataList[0]['Email'];
             const phone   = dataList[0]['Telephone'];
             const address = dataList[0]['Adresse'];
             setEmail(email)   
             setPhone(phone)
             setAddress(address)
-            
+            setContactTitle(contactTitle)
 
         }
     
@@ -84,21 +124,49 @@ const Footer = () => {
                 </div>
                 <div className="frame-14">
                     <div className="group-9">
-                    <div className="text-wrapper-10">Nos pages</div>
-                    <div className="text-wrapper-11">Accueil</div>
-                    <div className="text-wrapper-12">A propos</div>
-                    <div className="text-wrapper-13">Marketplace</div>
-                    <div className="text-wrapper-14">FAQ</div>
+                    <div className="text-wrapper-10">{ourPagesTitle}</div>
+                    <div className="text-wrapper-11">
+                        <Link href={homeLinkUrl}>
+                            {homeLinkText}
+                        </Link>    
+                    </div>
+                    <div className="text-wrapper-12">
+                        <Link href={aboutLinkUrl}>
+                            {aboutLinkText}
+                        </Link>    
+                    </div>
+                    <div className="text-wrapper-13">
+                        <Link href={marketPlaceLinkUrl}>
+                            {marketPlaceLinkText}
+                        </Link>    
+                    </div>
+                    <div className="text-wrapper-14">
+                        <Link href={faqLinkUrl}>
+                            {faqLinkText}
+                        </Link>    
+                    </div>
                     </div>
                     <div className="group-10">
-                    <div className="text-wrapper-10">Entreprise</div>
-                    <div className="text-wrapper-11">Equipes</div>
-                    <div className="text-wrapper-12">Partenaire</div>
-                    <div className="text-wrapper-13">CGU</div>
+                    <div className="text-wrapper-10">
+                        {ourCompanyTitle}
+                    </div>
+                    <div className="text-wrapper-11">
+                        <Link href={teamLinkUrl}>
+                            {teamLinkText}
+                        </Link>    
+                    </div>
+                    <div className="text-wrapper-12">
+                        <Link href={partnersLinkUrl}>
+                            {partnersLinkText}
+                        </Link>
+                    </div>
+                    <div className="text-wrapper-13">
+                        CGU
+                    </div>
                     </div>
                     <img className="line-2" alt="Line" src="/img/line-5.svg" />
                     <div className="group-11">
-                    <div className="text-wrapper-10">Contact</div>
+                    <div className="text-wrapper-10">{contactTitle}</div>  
                     <p className="text-wrapper-11">{phone}</p>
                     <div className="text-wrapper-12">{address}</div>
                     <div className="text-wrapper-13">{email}</div>
