3e96e59333ffacf9e89a19df523783f11219c1a3
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-29 15:28:37 +0100

    Modify html structure ans css of social network logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { db } from '../firebaseConfig';
 import { collection, getDocs } from 'firebase/firestore/lite';
 import { useAppContext } from '../context';
+import Link from 'next/link';
 
 
 const Footer = () => {
@@ -35,34 +36,45 @@ const Footer = () => {
 
     return (
         <>
-            <div className="frame-16">
-            <div className="frame-12">
-                <img className="logo-2" alt="Logo" src="/img/logo-1.png" />
-                <img className="add" alt="Frame" src="/img/frame-3352.svg" />
-                <p className="text-wrapper-9">Rendez physique vos œuvres digital</p>
-            </div>
-            <div className="frame-14">
-                <div className="group-9">
-                <div className="text-wrapper-10">Nos pages</div>
-                <div className="text-wrapper-11">Accueil</div>
-                <div className="text-wrapper-12">A propos</div>
-                <div className="text-wrapper-13">Marketplace</div>
-                <div className="text-wrapper-14">FAQ</div>
+            <div className="footer">
+                <div className="frame-12">
+                    <img className="logo-2" alt="Logo" src="/img/logo-IRA.png" />
+                    <div className="logo-social-network">
+                        <Link href="/">
+                            <img className="add" alt="Frame" src="/img/linkedin-logo.png" />
+                        </Link>
+                        <Link href="/">
+                            <img className="add" alt="Frame" src="/img/instagram-logo.png" />
+                        </Link>
+                        <Link href="/">
+                            <img className="add" alt="Frame" src="/img/twitter-logo.png" />
+                        </Link>
+                        
+                    </div>
+                    <p className="text-wrapper-9">Rendez physique vos œuvres digital</p>
                 </div>
-                <div className="group-10">
-                <div className="text-wrapper-10">Entreprise</div>
-                <div className="text-wrapper-11">Equipes</div>
-                <div className="text-wrapper-12">Partenaire</div>
-                <div className="text-wrapper-13">CGU</div>
+                <div className="frame-14">
+                    <div className="group-9">
+                    <div className="text-wrapper-10">Nos pages</div>
+                    <div className="text-wrapper-11">Accueil</div>
+                    <div className="text-wrapper-12">A propos</div>
+                    <div className="text-wrapper-13">Marketplace</div>
+                    <div className="text-wrapper-14">FAQ</div>
+                    </div>
+                    <div className="group-10">
+                    <div className="text-wrapper-10">Entreprise</div>
+                    <div className="text-wrapper-11">Equipes</div>
+                    <div className="text-wrapper-12">Partenaire</div>
+                    <div className="text-wrapper-13">CGU</div>
+                    </div>
+                    <img className="line-2" alt="Line" src="/img/line-5.svg" />
+                    <div className="group-11">
+                    <div className="text-wrapper-10">Contact</div>
+                    <p className="text-wrapper-11">{phone}</p>
+                    <div className="text-wrapper-12">{address}</div>
+                    <div className="text-wrapper-13">{email}</div>
+                    </div>
                 </div>
-                <img className="line-2" alt="Line" src="/img/line-5.svg" />
-                <div className="group-11">
-                <div className="text-wrapper-10">Contact</div>
-                <p className="text-wrapper-11">{phone}</p>
-                <div className="text-wrapper-12">{address}</div>
-                <div className="text-wrapper-13">{email}</div>
-                </div>
-            </div>
             </div>
             
         </>
