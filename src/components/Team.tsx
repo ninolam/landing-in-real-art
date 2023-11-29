ac048f8941a047f615c3b72c918bcd912a466100
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-29 10:57:16 +0100

    take member property team in Firestore

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -11,38 +11,40 @@ const Team = () => {
     //Get the language of the global context
     const {lang, setLang} = useAppContext()
 
-    const FIREBASE_TEAM_COLLECTION         = 'Team'
-    const FIREBASE_KEY_TEXT_1              = 'text1'
-    const FIREBASE_KEY_TEXT_2              = 'text2'
-    const FIREBASE_KEY_PROJECT_LEADER      = 'project_leader'
-    const FIREBASE_KEY_PROJECT_LEADER_NAME = 'project_leader_name'
-    let LANGUAGE                           = lang
+    const FIREBASE_TEAM_COLLECTION = 'Team'
+    const FIREBASE_KEY_TEXT_1      = 'text1'
+    const FIREBASE_KEY_TEXT_2      = 'text2'
+    const FIREBASE_KEY_ROLE        = 'role'
+    const FIREBASE_KEY_NAME        = 'name'
+    let LANGUAGE                   = lang
 
-    const [text1, setText1]                         = useState<string>('');
-    const [text2, setText2]                         = useState<string>('');
-    const [projectLeader, setProjectLeader]         = useState<string>('');
-    const [projectLeaderName, setProjectLeaderName] = useState<string>('');
-    const [members, setMembers]                     = useState<[]>([]);
+    const [text1, setText1] = useState<string>('');
+    const [text2, setText2] = useState<string>('');
+    const [role, setRole]   = useState<string>('');
+    const [name, setName]   = useState<string>('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [members, setMembers] = useState<[]>([]);
 
     useEffect(() => {
       const fetchText = async () => {
           const teamCollection = collection(db, FIREBASE_TEAM_COLLECTION);
           const teamDocuments  = await getDocs(teamCollection);
           const teamData       = teamDocuments.docs.map(doc => doc.data());
-          setMembers(teamData[0]['members'])
-          console.log(teamData[0]['members'])  
-          //Index 0 ===> Team Text
-          setText1(teamData[0][FIREBASE_KEY_TEXT_1][LANGUAGE])
-          setText2(teamData[0][FIREBASE_KEY_TEXT_2][LANGUAGE])
-          setProjectLeader(teamData[0][FIREBASE_KEY_PROJECT_LEADER][LANGUAGE])
-          setProjectLeaderName(teamData[0][FIREBASE_KEY_PROJECT_LEADER_NAME][LANGUAGE])
+          const members_ = teamData[0]['members'] 
+          setMembers(members_)
+          console.log(members_)
+
+          setText1(members_[currentIndex][FIREBASE_KEY_TEXT_1])
+          setText2(members_[currentIndex][FIREBASE_KEY_TEXT_2])
+          setRole(members_[currentIndex][FIREBASE_KEY_ROLE])
+          setName(members_[currentIndex][FIREBASE_KEY_NAME])
       }
   
       fetchText();
       
     }, [lang]);
   
-    const [currentIndex, setCurrentIndex] = useState(0);
+    
 
     const handleArrowClick = (direction: string) => {
       if (direction === 'right') {
@@ -55,7 +57,7 @@ const Team = () => {
 
     return (
         <div className="team">
-          <TeamMember name="Tim Roy" photo="test" role="Project leader" text1="text1" text2="text2"/>
+          <TeamMember name={name} photo="test" role={role} text1={text1} text2={text2}/>
           <div className="scrollTeam">
                   <img alt="left" src="/img/icons8-arrow-left.png" onClick={() => handleArrowClick('left')}/>
                   <img alt="right" src="/img/icons8-arrow-right.png" onClick={() => handleArrowClick('right')}/>
