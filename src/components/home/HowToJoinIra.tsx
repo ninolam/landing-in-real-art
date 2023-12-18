1f984acd3112590888c9ba168c0d7de1ad4a14f6
Gilles Bruno <gilles_bruno@gmx.fr>
2023-12-18 18:22:30 +0100

    simplify code

--- src/components/home/HowToJoinIra.tsx
+++ src/components/home/HowToJoinIra.tsx
@@ -16,13 +16,6 @@ const HowToJoinIra = () => {
   
     const FIREBASE_JOIN_IRA_COLLECTION = 'JoinIRA'
     
-    const [joinIra, setJoinIra]           = useState<string>('');
-    const [startIra, setStartIra]         = useState<string>('');
-    const [joinIraLink, setJoinIraLink]   = useState<string>('');
-    const [startIraLink, setStartIraLink] = useState<string>('');
-    const [text1, setText1]               = useState<string>('');
-    const [text2, setText2]               = useState<string>('');
-    const [headerText, setHeaderText]     = useState<string>('');
     const defaultJoinIraText = {
       text1: defaultLangObject,
       text2: defaultLangObject,
@@ -48,37 +41,13 @@ const HowToJoinIra = () => {
         
         //Index 0 ===> joinIRA Buttons
         setJoinIraDataButton(joinIRAData[0] as JoinIraDataButton)
-        setStartIra(joinIRAData[0].StartIRA[lang])
-        setJoinIra(joinIRAData[0].JoinIRA[lang])
-        setJoinIraLink(joinIRAData[0].JoinIRALink)
-        setStartIraLink(joinIRAData[0].StartIRALink)
   
         //Index 1 ===> joinIRA Text
         setJoinIraDataText(joinIRAData[1] as JoinIraDataText)
-        setText1(joinIRAData[1].text1[lang])
-        setText2(joinIRAData[1].text2[lang])
-        setHeaderText(joinIRAData[1].headerText[lang])
     }  
     fetchData();
     }, [])
   
-  
-    useEffect(() => {
-      const fetchText = async () => {
-        // Index 0 ===> joinIRA Buttons
-        setStartIra(joinIraDataButton.StartIRA[lang_])
-        setJoinIra(joinIraDataButton.JoinIRA[lang_])
-        setJoinIraLink(joinIraDataButton.JoinIRALink)
-        setStartIraLink(joinIraDataButton.StartIRALink)
-  
-        // Index 1 ===> joinIRA Text
-        setText1(joinIraDataText.text1[lang_])
-        setText2(joinIraDataText.text2[lang_])
-        setHeaderText(joinIraDataText.headerText[lang_])
-      }
-      
-      fetchText();
-    }, [lang]);
   
     return (
         <div className="feature">
@@ -92,23 +61,23 @@ const HowToJoinIra = () => {
         </div>
         <div className="frame-48095733">
           <div className="frame-303">
-            <div className="comment">Comment ?</div>
+            <div className="comment">{joinIraDataText.headerText[lang_]}</div>
             <div
               className="ira-ambition">
-              {text1}
+              {joinIraDataText.text1[lang_]}
               <br />
               <br />
-              {text2}
+              {joinIraDataText.text2[lang_]}
             </div>
             <div className="link-button">
               <div className="button2">
-                <Link href={joinIraLink}>
-                  <div className="rejoindre-ira2">{joinIra}</div>
+                <Link href={joinIraDataButton.JoinIRALink}>
+                  <div className="rejoindre-ira2">{joinIraDataButton.JoinIRA[lang_]}</div>
                 </Link>  
               </div>
               <div className="button3">
-                <Link href={joinIraLink}>
-                  <div className="je-d-marre2">{startIra}</div>
+                <Link href={joinIraDataButton.StartIRALink}>
+                  <div className="je-d-marre2">{joinIraDataButton.StartIRA[lang_]}</div>
                 </Link>  
               </div>
             </div>
