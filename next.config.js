e1c987957ee53bf7ca6005a4ab6b24e98332a64a
Gilles Bruno <gilles_bruno@gmx.fr>
2023-12-21 15:35:42 +0100

    Redirect automatically to 'home' route

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
     images: {
         domains: ["firebasestorage.googleapis.com"]
-    }
+    },
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/home',
+            permanent: true, // set to false if the redirect is temporary
+          },
+        ]
+      }
 }
 
 module.exports = nextConfig
