c34f47575d04f5ee89fd67b8b2ef3b50bfc5f38e
Gilles Bruno <gilles_bruno@gmx.fr>
2024-01-08 17:18:34 +0100

    DropPanel SCSS module

--- src/components/presale/DropPanel/DropPanel.tsx
+++ src/components/presale/DropPanel/DropPanel.tsx
@@ -1,115 +1,104 @@
 "use client"
 import Arrow21 from "../Arrows21"
+import styles from './DropPanel.module.scss'
 
 function DropPanel() {
   return (
-    <div className="frame-5">
-    <div className="frame-6">
-        <div className="frame-7">
-        <div className="text-wrapper-3">Fin du drop dans</div>
+    <div className={styles["frame-5"]}>
+    <div className={styles["frame-6"]}>
+        <div className={styles["frame-7"]}>
+        <div className={styles["text-wrapper-3"]}>Fin du drop dans</div>
         </div>
-        <div className="frame-8">
-        <div className="text-wrapper-4">65J 20H</div>
+        <div className={styles["frame-8"]}>
+        <div className={styles["text-wrapper-4"]}>65J 20H</div>
         </div>
     </div>
-    <div className="frame-9">
-        <div className="frame-10">
-        <div className="frame-11">
-            <div className="frame-3">
-            <img
-                className="rectangle-3"
-                alt="Rectangle"
+    <div className={styles["frame-9"]}>
+        <div className={styles["frame-10"]}>
+        <div className={styles["frame-11"]}>
+            <div className={styles["frame-3"]}>
+            <img className={styles["rectangle-3"]} alt=""
                 src="https://cdn.animaapp.com/projects/655cacc35c6faf1eb176ebc4/releases/657325d4ed28baa6bd577524/img/rectangle-39770.svg"
             />
-            <div className="frame-12">
-                <div className="text-wrapper-5">Détail</div>
-                <Arrow21 className="arrow" />
+            <div className={styles["frame-12"]}>
+                <div className={styles["text-wrapper-5"]}>Détail</div>
+                <Arrow21 className={styles["arrow"]} />
             </div>
             </div>
-            <button className="button-2">
-            <div className="text-wrapper-6">Acquérir</div>
+            <button className={styles["button-2"]}>
+            <div className={styles["text-wrapper-6"]}>Acquérir</div>
             </button>
         </div>
-        <div className="frame-11">
-            <div className="frame-3">
-            <img
-                className="rectangle-3"
-                alt="Rectangle"
+        <div className={styles["frame-11"]}>
+            <div className={styles["frame-3"]}>
+            <img className={styles["rectangle-3"]} alt=""
                 src="https://cdn.animaapp.com/projects/655cacc35c6faf1eb176ebc4/releases/657325d4ed28baa6bd577524/img/rectangle-39770-1.svg"
             />
-            <div className="frame-12">
-                <div className="text-wrapper-5">Détail</div>
-                <Arrow21 className="arrow" />
+            <div className={styles["frame-12"]}>
+                <div className={styles["text-wrapper-5"]}>Détail</div>
+                <Arrow21 className={styles["arrow"]} />
             </div>
             </div>
-            <button className="button-2">
-            <div className="text-wrapper-6">Acquérir</div>
+            <button className={styles["button-2"]}>
+            <div className={styles["text-wrapper-6"]}>Acquérir</div>
             </button>
         </div>
-        <div className="frame-11">
-            <div className="frame-3">
-            <img
-                className="rectangle-3"
-                alt="Rectangle"
+        <div className={styles["frame-11"]}>
+            <div className={styles["frame-3"]}>
+            <img className={styles["rectangle-3"]} alt=""
                 src="https://cdn.animaapp.com/projects/655cacc35c6faf1eb176ebc4/releases/657325d4ed28baa6bd577524/img/rectangle-39770-2.svg"
             />
-            <div className="frame-12">
-                <div className="text-wrapper-5">Détail</div>
-                <Arrow21 className="arrow" />
+            <div className={styles["frame-12"]}>
+                <div className={styles["text-wrapper-5"]}>Détail</div>
+                <Arrow21 className={styles["arrow"]} />
             </div>
             </div>
-            <button className="button-2">
-            <div className="text-wrapper-6">Acquérir</div>
+            <button className={styles["button-2"]}>
+            <div className={styles["text-wrapper-6"]}>Acquérir</div>
             </button>
         </div>
         </div>
-        <div className="frame-10">
-            <div className="frame-11">
-                <div className="frame-3">
-                <img
-                    className="rectangle-3"
-                    alt="Rectangle"
+        <div className={styles["frame-10"]}>
+            <div className={styles["frame-11"]}>
+                <div className={styles["frame-3"]}>
+                <img className={styles["rectangle-3"]} alt=""
                     src="https://cdn.animaapp.com/projects/655cacc35c6faf1eb176ebc4/releases/657325d4ed28baa6bd577524/img/rectangle-39770-3.svg"
                 />
-                <div className="frame-12">
-                    <div className="text-wrapper-5">Détail</div>
-                    <Arrow21 className="arrow" />
+                <div className={styles["frame-12"]}>
+                    <div className={styles["text-wrapper-5"]}>Détail</div>
+                    <Arrow21 className={styles["arrow"]} />
                 </div>
                 </div>
-                <button className="button-2">
-                <div className="text-wrapper-6">Acquérir</div>
+                <button className={styles["button-2"]}>
+                <div className={styles["text-wrapper-6"]}>Acquérir</div>
                 </button>
             </div>
-            <div className="frame-11">
-                <div className="frame-3">
-                <img
-                    className="rectangle-3"
-                    alt="Rectangle"
+            <div className={styles["frame-11"]}>
+                <div className={styles["frame-3"]}>
+                <img className={styles["rectangle-3"]} alt=""
                     src="https://cdn.animaapp.com/projects/655cacc35c6faf1eb176ebc4/releases/657325d4ed28baa6bd577524/img/rectangle-39770-4.svg"
                 />
-                <div className="frame-12">
-                    <div className="text-wrapper-5">Détail</div>
-                    <Arrow21 className="arrow" />
+                <div className={styles["frame-12"]}>
+                    <div className={styles["text-wrapper-5"]}>Détail</div>
+                    <Arrow21 className={styles["arrow"]} />
                 </div>
                 </div>
-                <button className="button-2">
-                <div className="text-wrapper-6">Acquérir</div>
+                <button className={styles["button-2"]}>
+                <div className={styles["text-wrapper-6"]}>Acquérir</div>
                 </button>
             </div>
-            <div className="frame-11">
-                <div className="frame-3">
-                <img
-                    className="rectangle-3"
-                    alt="Rectangle"
+            <div className={styles["frame-11"]}>
+                <div className={styles["frame-3"]}>
+                <img className={styles["rectangle-3"]} alt=""
                     src="https://cdn.animaapp.com/projects/655cacc35c6faf1eb176ebc4/releases/657325d4ed28baa6bd577524/img/rectangle-39770-5.svg"
                 />
-                <div className="frame-12">
-                    <div className="text-wrapper-5">Détail</div>
-                    <Arrow21 className="arrow" />
+                <div className={styles["frame-12"]}>
+                    <div className={styles["text-wrapper-5"]}>Détail</div>
+                    <Arrow21 className={styles["arrow"]} />
                 </div>
                 </div>
-                <button className="button-2">
-                <div className="text-wrapper-6">Acquérir</div>
+                <button className={styles["button-2"]}>
+                <div className={styles["text-wrapper-6"]}>Acquérir</div>
                 </button>
             </div>
         </div>
