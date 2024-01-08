d457d3a42e64f981ad051889cd999903429e7a72
Gilles Bruno <gilles_bruno@gmx.fr>
2024-01-08 17:02:33 +0100

    Buying process presale SCSS module

--- src/components/presale/PresalePros/PresalePros.tsx
+++ src/components/presale/PresalePros/PresalePros.tsx
@@ -8,6 +8,7 @@ import { useAppContext } from "../../../context";
 import { Lang, defaultLangObject } from "../../../types/types";
 import { PresaleProsButtons, PresaleProsTexts } from "../../../types/presale.types";
 import parse from 'html-react-parser';
+import styles from './PresalePros.module.scss'
 
 function PresalePros() {
 
@@ -56,64 +57,64 @@ function PresalePros() {
       }, [])
 
   return (
-    <div className="frame-13">
-    <div className="frame-wrapper">
-        <div className="frame-14">
-        <div className="avantages-d-achats">
+    <div className={styles["frame-13"]}>
+    <div className={styles["frame-wrapper"]}>
+        <div className={styles["frame-14"]}>
+        <div className={styles["avantages-d-achats"]}>
             {parse(presaleProsTexts.mainTitle[lang_])}
         </div>
-        <p className="ne-louper-pas-la">
+        <p className={styles["ne-louper-pas-la"]}>
             {parse(presaleProsTexts.mainDescription[lang_])}
         </p>
         </div>
     </div>
-    <div className="frame-15">
-        <div className="frame-16">
-        <div className="frame-17">
-            <div className="rectangle-4" />
-            <VuesaxLinearStatusUp1 className="vuesax-linear-status" />
+    <div className={styles["frame-15"]}>
+        <div className={styles["frame-16"]}>
+        <div className={styles["frame-17"]}>
+            <div className={styles["rectangle-4"]} />
+            <VuesaxLinearStatusUp1 className={styles["vuesax-linear-status"]} />
         </div>
-        <div className="frame-18">
-            <div className="heading-2">{parse(presaleProsTexts.buyArtWorkTitle[lang_])}</div>
-            <p className="paragraph">{parse(presaleProsTexts.buyArtWorkDescription[lang_])}</p>
+        <div className={styles["frame-18"]}>
+            <div className={styles["heading-2"]}>{parse(presaleProsTexts.buyArtWorkTitle[lang_])}</div>
+            <p className={styles["paragraph"]}>{parse(presaleProsTexts.buyArtWorkDescription[lang_])}</p>
         </div>
-        <div className="heading-wrapper">
+        <div className={styles["heading-wrapper"]}>
             <Link href={presaleProsButtons.buyArtWorkLink}>
-                <div className="heading-3">{presaleProsButtons.buyArtWork[lang_]}</div>
+                <div className={styles["heading-3"]}>{presaleProsButtons.buyArtWork[lang_]}</div>
             </Link>
         </div>
         </div>
-        <div className="frame-19">
-        <div className="frame-3">
-            <div className="frame-16">
-            <div className="frame-17">
-                <div className="rectangle-4" />
-                <VuesaxLinearStatusUp1 className="vuesax-linear-status" />
+        <div className={styles["frame-19"]}>
+        <div className={styles["frame-3"]}>
+            <div className={styles["frame-16"]}>
+            <div className={styles["frame-17"]}>
+                <div className={styles["rectangle-4"]} />
+                <VuesaxLinearStatusUp1 className={styles["vuesax-linear-status"]} />
             </div>
-            <div className="frame-18">
-                <div className="heading-2">{parse(presaleProsTexts.exclusiveBenefitsTitle[lang_])}</div>
-                <p className="paragraph">{parse(presaleProsTexts.exclusiveBenefitsDescription[lang_])}</p>
+            <div className={styles["frame-18"]}>
+                <div className={styles["heading-2"]}>{parse(presaleProsTexts.exclusiveBenefitsTitle[lang_])}</div>
+                <p className={styles["paragraph"]}>{parse(presaleProsTexts.exclusiveBenefitsDescription[lang_])}</p>
             </div>
-            <div className="heading-wrapper">
+            <div className={styles["heading-wrapper"]}>
                 <Link href={presaleProsButtons.exclusiveBenefitsLink}>
-                    <div className="heading-3">{presaleProsButtons.exclusiveBenefits[lang_]}</div>
+                    <div className={styles["heading-3"]}>{presaleProsButtons.exclusiveBenefits[lang_]}</div>
                 </Link>    
             </div>
             </div>
         </div>
         </div>
-        <div className="frame-16">
-        <div className="frame-17">
-            <div className="rectangle-4" />
-            <VuesaxLinearStatusUp1 className="vuesax-linear-status" />
+        <div className={styles["frame-16"]}>
+        <div className={styles["frame-17"]}>
+            <div className={styles["rectangle-4"]} />
+            <VuesaxLinearStatusUp1 className={styles["vuesax-linear-status"]} />
         </div>
-        <div className="frame-18">
-            <div className="heading-2">{parse(presaleProsTexts.bonusTitle[lang_])}</div>
-            <p className="paragraph">{parse(presaleProsTexts.bonusDescription[lang_])}</p>
+        <div className={styles["frame-18"]}>
+            <div className={styles["heading-2"]}>{parse(presaleProsTexts.bonusTitle[lang_])}</div>
+            <p className={styles["paragraph"]}>{parse(presaleProsTexts.bonusDescription[lang_])}</p>
         </div>
-        <div className="heading-wrapper">
+        <div className={styles["heading-wrapper"]}>
             <Link href={presaleProsButtons.bonusLink}>
-                <div className="heading-3">{presaleProsButtons.bonus[lang_]}</div>
+                <div className={styles["heading-3"]}>{presaleProsButtons.bonus[lang_]}</div>
             </Link>    
         </div>
         </div>
