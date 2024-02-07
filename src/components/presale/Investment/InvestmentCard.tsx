2e8c8c4bd9fad0d9ad39f98217ed175e25b39eb2
Gilles Bruno <gilles_bruno@gmx.fr>
2024-02-07 16:39:16 +0100

    Change investment block with ratio card increase

--- src/components/presale/Investment/InvestmentCard.tsx
+++ src/components/presale/Investment/InvestmentCard.tsx
@@ -1,6 +1,7 @@
 // Card.js
 import React, { useState } from 'react'
 import styles from './Investment.module.scss'
+import InvestmentCardDetail from './InvestmentCardDetail';
 
 
 export interface InvestmentCardProps {
@@ -16,36 +17,50 @@ const InvestmentCard = ({ number, title1, title2, details, imageSrc }: Investmen
   const [isHovered, setIsHovered] = useState(false);
 
   let borderRadius= ''
+  let styleCard = ''
   if (number === '01' ) {
     borderRadius = '50px 0px 0px 50px'
-    if (window.innerWidth < 600) {
+    if (window.innerWidth < 1000) {
       borderRadius = '50px 50px 0px 0px'
     }
+    styleCard = styles.card1
   }
   else if (number === '03' ) {
     borderRadius = '0px 50px 50px 0px'
-    if (window.innerWidth < 600) {
+    if (window.innerWidth < 1000) {
       borderRadius = '0px 0px 50px 50px'
     }  
+    styleCard = styles.card3
   }
+  else if (number === '03' ) {
+    styleCard = styles.card2
+  }  
 
   return (
     <div
-
-        className={styles.card}
+        className=''
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        style={{ backgroundImage: `url(${imageSrc})`, borderRadius: `${borderRadius}` }}
+        style={{ backgroundImage: `url(${imageSrc})`, backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: `${borderRadius}` }}
     >
         
         <div className={styles.cardTitleContainer}>
-            <div className={styles.cardTitleNumber}>{number}</div>
-            <div className={styles.cardTitle}>
-                <div className={styles.cardTitle1}>{title1}</div>
-                <div className={styles.cardTitle2}>{title2}</div>
+            <div>
+              <span className={styles.cardTitleNumber}>{number}</span>
+              <span className={styles.cardTitle1}>{title1}</span>
+              <span className={styles.cardTitle2}>{title2}</span>
             </div>
+            {
+              /*
+                <div className={styles.cardTitle}>
+                  <div className={styles.cardTitle1}>{title1}</div>
+                  <div className={styles.cardTitle2}>{title2}</div>
+                </div>
+              */
+            }
+            
         </div>
-      {isHovered && <div className={styles.details}>{details}</div>}
+      {/*isHovered && <InvestmentCardDetail/>*/}
 
     </div>
   );
