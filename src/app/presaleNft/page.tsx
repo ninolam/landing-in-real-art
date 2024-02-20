243c70401436b557112f715448f788f8008aaa61
Gilles Bruno <gilles_bruno@gmx.fr>
2024-02-20 15:52:42 +0100

    Presale Nft collection

--- src/app/presaleNft/page.tsx
+++ src/app/presaleNft/page.tsx
@@ -42,7 +42,7 @@ export default function PresalePage() {
                         <JoinMovementNft/>
                         <Menu/>
                         <AboutArtist/>
-                        {/*<NftCollection/>*/}
+                        <NftCollection/>
                         <GetReadyToBuy/>
                         <FooterMobile containsEmail={true}/>
                     </>
@@ -55,7 +55,7 @@ export default function PresalePage() {
                         <JoinMovementNft/>
                         <Menu/>
                         <AboutArtist/>
-                        {/*<NftCollection/>*/}
+                        <NftCollection/>
                         <GetReadyToBuy/>
                         <Footer/>
                     </>
--- src/components/presaleNft/NftCollection/NftCollection.tsx
+++ src/components/presaleNft/NftCollection/NftCollection.tsx
@@ -3,60 +3,74 @@ import styles from './NftCollection.module.scss'
 
 const NftCollection = () => {
   return (
-    <div className={styles.nftCollection}>
-        <div className={styles.nftCard}>
-            <div className={styles.nftCardBackground}>
-              NFT
-            </div>    
-            <div className={styles.nftCardInfo}>
-              
-            </div>    
-            <a className="Button Button--gold" href="/nfts/1">Buy</a>
+    <div className={styles.nftCollectionContainer}>
+        <div className={styles.header}>
+            <div className={styles.nftCollectionTitle}>Collection</div>
+            <div className={styles.nftCollectionDesc}>
+                Discover the collection of NFT works <br/>available online
+            </div>
+            <div className={styles.category}>
+                <div className={styles.categoryFrame}>
+                    <div className={styles.categoryLeloluce}>Leloluce</div>
+                </div>
+            </div>
         </div>
-        <div className={styles.nftCard}>
-            <div className={styles.nftCardBackground}>
-            NFT
-            </div>    
-            <div className={styles.nftCardInfo}>
-              
-            </div>    
-            <a className="Button Button--gold" href="/nfts/1">Buy</a>
-        </div>
-        <div className={styles.nftCard}>
-            <div className={styles.nftCardBackground} >
-            NFT
-            </div>    
-            <div className={styles.nftCardInfo}>
-              
-            </div>    
-            <a className="Button Button--gold" href="/nfts/1">Buy</a>
-        </div>
-        <div className={styles.nftCard}>
-            <div className={styles.nftCardBackground} >
 
-            </div>    
-            <div className={styles.nftCardInfo}>
-              
-            </div>    
-            <a className="Button Button--gold" href="/nfts/1">Buy</a>
-        </div>
-        <div className={styles.nftCard}>
-            <div className={styles.nftCardBackground} >
+        <div className={styles.nftCollection}>
+            <div className={styles.nftCard}>
+                <div className={styles.nftCardBackground} style={{backgroundImage: `url('img/presaleNft/collection3.png')`}}>
+                
+                </div>    
+                <div className={styles.nftCardInfo}>
+                
+                </div>    
+                <a className="Button Button--gold" href="/nfts/1">Buy</a>
+            </div>
+            <div className={styles.nftCard}>
+                <div className={styles.nftCardBackground} style={{backgroundImage: `url('img/presaleNft/collection3.png')`}}>
+                
+                </div>    
+                <div className={styles.nftCardInfo}>
+                
+                </div>    
+                <a className="Button Button--gold" href="/nfts/1">Buy</a>
+            </div>
+            <div className={styles.nftCard}>
+                <div className={styles.nftCardBackground} style={{backgroundImage: `url('img/presaleNft/collection3.png')`}}>
+                
+                </div>    
+                <div className={styles.nftCardInfo}>
+                
+                </div>    
+                <a className="Button Button--gold" href="/nfts/1">Buy</a>
+            </div>
+            <div className={styles.nftCard}>
+                <div className={styles.nftCardBackground} style={{backgroundImage: `url('img/presaleNft/collection3.png')`}}>
 
-            </div>    
-            <div className={styles.nftCardInfo}>
-              
-            </div>    
-            <a className="Button Button--gold" href="/nfts/1">Buy</a>
-        </div>
-        <div className={styles.nftCard}>
-            <div className={styles.nftCardBackground} >
+                </div>    
+                <div className={styles.nftCardInfo}>
+                
+                </div>    
+                <a className="Button Button--gold" href="/nfts/1">Buy</a>
+            </div>
+            <div className={styles.nftCard}>
+                <div className={styles.nftCardBackground} style={{backgroundImage: `url('img/presaleNft/collection3.png')`}}>
 
-            </div>    
-            <div className={styles.nftCardInfo}>
-              
-            </div>    
-            <a className="Button Button--gold" href="/nfts/1">Buy</a>
+                </div>    
+                <div className={styles.nftCardInfo}>
+                
+                </div>    
+                <a className="Button Button--gold" href="/nfts/1">Buy</a>
+            </div>
+            <div className={styles.nftCard}>
+                <div className={styles.nftCardBackground} style={{backgroundImage: `url('img/presaleNft/collection3.png')`}}>
+
+                </div>    
+                <div className={styles.nftCardInfo}>
+                
+                </div>    
+                <a className="Button Button--gold" href="/nfts/1">Buy</a>
+            </div>
         </div>
     </div>
   )
