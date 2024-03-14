6ac0239b2b8924ebb4aa14871c621cccc4e1329f
Gilles Bruno <gilles_bruno@gmx.fr>
2024-03-14 16:01:32 +0100

    Better nft page

--- src/components/presaleNft/JoinMovement/JoinMovementNftGallery.tsx
+++ src/components/presaleNft/JoinMovement/JoinMovementNftGallery.tsx
@@ -5,19 +5,19 @@ const JoinMovementNftGallery = () => {
   return (
         <div className={styles["joinMovementNftGallery"]}>
             <div className={styles["joinMovementNftGalleryColumn1"]}>
-                <img className={styles["joinMovementNftGalleryItem11"]} src="img/presaleNft/heroImage15.png" />
-                <img className={styles["joinMovementNftGalleryItem13"]} src="img/presaleNft/heroImage20.png" />
-                <img className={styles["joinMovementNftGalleryItem12"]} src="img/presaleNft/heroImage18.png" />
+                <img className={styles["joinMovementNftGalleryItem11"]} src="img/presaleNft/heroSection/heroLeloluce1.jpg" />
+                <img className={styles["joinMovementNftGalleryItem13"]} src="img/presaleNft/heroSection/heroLeloluce2.jpg" />
+                <img className={styles["joinMovementNftGalleryItem12"]} src="img/presaleNft/heroSection/heroLeloluce3.jpg" />
             </div>
             
             <div className={styles["joinMovementNftGalleryColumn2"]}>
-                <img className={styles["joinMovementNftGalleryItem21"]} src="img/presaleNft/heroImage17.png" />
-                <img className={styles["joinMovementNftGalleryItem23"]} src="img/presaleNft/heroImage19.png" />
-                <img className={styles["joinMovementNftGalleryItem22"]} src="img/presaleNft/heroImage16.png" />
+                <img className={styles["joinMovementNftGalleryItem21"]} src="img/presaleNft/heroSection/heroLeloluce4.jpg" />
+                <img className={styles["joinMovementNftGalleryItem23"]} src="img/presaleNft/heroSection/heroLeloluce5.jpg" />
+                <img className={styles["joinMovementNftGalleryItem22"]} src="img/presaleNft/heroSection/heroLeloluce6.jpg" />
             </div>
             
             <div className={styles["joinMovementNftGalleryColumn3"]}>
-                <img className={styles["joinMovementNftGalleryItem31"]} src="img/presaleNft/heroImage21.png" />
+                <img className={styles["joinMovementNftGalleryItem31"]} src="img/presaleNft/heroSection/heroLeloluce7.jpg" />
             </div>
             
         </div>
