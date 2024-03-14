9579cb775b7ef5f9a483e458ca189f14fae47e99
Gilles Bruno <gilles_bruno@gmx.fr>
2024-03-14 14:22:17 +0100

    Change image for leloluce

--- src/components/presaleNft/JoinMovement/JoinMovementNftGallery950.tsx
+++ src/components/presaleNft/JoinMovement/JoinMovementNftGallery950.tsx
@@ -5,13 +5,13 @@ const JoinMovementNftGallery950 = () => {
   return (
         <div className={styles["joinMovementNftGallery"]}>
             <div className={styles["joinMovementNftGalleryColumn1"]}>
-                <img className={styles["joinMovementNftGalleryItem11"]} src="img/presaleNft/heroImage15.png" />
+                <img className={styles["joinMovementNftGalleryItem11"]} src="img/presaleNft/heroSection/heroLeloluce1.jpg" />
                 
                 
             </div>
             
             <div className={styles["joinMovementNftGalleryColumn2"]}>
-                <img className={styles["joinMovementNftGalleryItem21"]} src="img/presaleNft/heroImage17.png" />
+                <img className={styles["joinMovementNftGalleryItem21"]} src="img/presaleNft/heroSection/heroLeloluce2.jpg" />
                 
             </div>            
         </div>
