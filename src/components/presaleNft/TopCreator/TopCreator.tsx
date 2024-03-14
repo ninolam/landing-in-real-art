d523046f9094d7b374239fd2268d9ad54752acd9
Gilles Bruno <gilles_bruno@gmx.fr>
2024-03-14 10:48:01 +0100

    Top creators block

--- src/components/presaleNft/TopCreator/TopCreator.tsx
+++ src/components/presaleNft/TopCreator/TopCreator.tsx
@@ -30,44 +30,44 @@ const TopCreator = () => {
 
         <div className={styles.topCreatorGallery}>
             {/* Creator 1*/}
-            <div className={styles.card}>
+            <div className={styles.card1}>
                 <div className={styles.cardHeader}>
                     <img src="img/presaleNft/creator1Header.png" alt="Pattern Background" className={styles.cardBackground}/>
                     <div className={styles.cardProfile}>
-                        <img src="img/presaleNft/photoArtist3.png" alt="" className={styles.profileImage}/>
+                        <img src={topCreators.card1.urlPhoto} alt="" className={styles.profileImage}/>
                         <h1 className={styles.profileName}>{topCreators?.card1.artistName}</h1>
                         <p className={styles.profileDescription}>{parse(topCreators?.card1.artistDescription[lang_])}</p>
                     </div>
                 </div>
-                <div className={styles.cardFooter}>
+                <div className={styles.cardFooter1}>
                     <Link href="/presale" className={styles.moreInfo}>{buttons?.discoverRwa[lang_]}</Link>
                 </div>
             </div>
             {/* Creator 2*/}
-            <div className={styles.card}>
+            <div className={styles.card2}>
                 <div className={styles.cardHeader}>
                     <img src="img/presaleNft/creator2Header.png" alt="Pattern Background" className={styles.cardBackground}/>
                     <div className={styles.cardProfile}>
-                        <img src="img/presaleNft/photoArtist3.png" alt="" className={styles.profileImage}/>
+                        <img src={topCreators.card2.urlPhoto} alt="" className={styles.profileImage}/>
                         <h1 className={styles.profileName}>{topCreators?.card2.artistName}</h1>
                         <p className={styles.profileDescription}>{parse(topCreators?.card2.artistDescription[lang_])}</p>
                     </div>
                 </div>
-                <div className={styles.cardFooter}>
+                <div className={styles.cardFooter2}>
                     <Link href="/presale" className={styles.moreInfo}>{buttons?.discoverRwa[lang_]}</Link>
                 </div>
             </div>
             {/* Creator 3*/}
-            <div className={styles.card}>
+            <div className={styles.card3}>
                 <div className={styles.cardHeader}>
                     <img src="img/presaleNft/creator3Header.png" alt="Pattern Background" className={styles.cardBackground}/>
                     <div className={styles.cardProfile}>
-                        <img src="img/presaleNft/photoArtist3.png" alt="John Wick" className={styles.profileImage}/>
+                        <img src={topCreators.card3.urlPhoto} alt="John Wick" className={styles.profileImage}/>
                         <h1 className={styles.profileName}>{topCreators?.card3?.artistName}</h1>
                         <p className={styles.profileDescription}>{parse(topCreators?.card3?.artistDescription[lang_])}</p>
                     </div>
                 </div>
-                <div className={styles.cardFooter}>
+                <div className={styles.cardFooter3}>
                     <Link href="/presale" className={styles.moreInfo}>{buttons?.discoverRwa[lang_]}</Link>
                 </div>
             </div>
