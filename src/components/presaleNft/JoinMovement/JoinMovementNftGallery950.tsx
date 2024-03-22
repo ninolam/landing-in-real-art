e67588df18a4f1a6c816cb9b3fb9a0c25f08f467
ninolam <lamoureux.nino@gmail.com>
2024-03-22 01:20:58 +0100

    fix lint

--- src/components/presaleNft/JoinMovement/JoinMovementNftGallery950.tsx
+++ src/components/presaleNft/JoinMovement/JoinMovementNftGallery950.tsx
@@ -5,14 +5,11 @@ const JoinMovementNftGallery950 = () => {
   return (
         <div className={styles["joinMovementNftGallery"]}>
             <div className={styles["joinMovementNftGalleryColumn1"]}>
-                <img className={styles["joinMovementNftGalleryItem11"]} src="img/presaleNft/heroSection/heroLeloluce1.jpg" />
-                
-                
+                <img alt='join mouvement' className={styles["joinMovementNftGalleryItem11"]} src="img/presaleNft/heroSection/heroLeloluce1.jpg" />            
             </div>
             
             <div className={styles["joinMovementNftGalleryColumn2"]}>
-                <img className={styles["joinMovementNftGalleryItem21"]} src="img/presaleNft/heroSection/heroLeloluce2.jpg" />
-                
+                <img alt='join mouvement' className={styles["joinMovementNftGalleryItem21"]} src="img/presaleNft/heroSection/heroLeloluce2.jpg" />
             </div>            
         </div>
   )
